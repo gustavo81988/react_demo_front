2572320a968c15753f81c88802c0351d57a9b939
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-01 15:18:11 -0400

    avance

--- src/App.js
+++ src/App.js
@@ -1,221 +1,41 @@
-import React from 'react';
-import Header from './Components/Header';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import { makeStyles } from 'tss-react/mui';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Textfield from './Components/FormsUI/Textfield';
-import Select from './Components/FormsUI/Select';
-import DateTimePicker from './Components/FormsUI/DataTimePicker';
-import Checkbox from './Components/FormsUI/Checkbox';
-import Button from './Components/FormsUI/Button';
-import countries from './data/countries.json';
-
-const useStyles = makeStyles()((theme) => {
-  return {
-    formWrapper: {
-      marginTop: theme.spacing(5),
-      marginBottom: theme.spacing(8),
-    }
-  };
-});
-
-const INITIAL_FORM_STATE = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  phone: '',
-  addressLine1: '',
-  addressLine2: '',
-  city: '',
-  state: '',
-  country: '',
-  arrivealDate: '',
-  departureDate: '',
-  message: '',
-  termsOfService: false
-};
-
-const FORM_VALIDATION = Yup.object().shape({
-  firstName: Yup.string()
-    .required('Required'),
-  lastName: Yup.string()
-    .required('Required'),
-  email: Yup.string()
-    .email('Invalid email.')
-    .required('Required'),
-  phone: Yup.number()
-    .integer()
-    .typeError('Please enter a valid phone number')
-    .required('Required'),
-  addressLine1: Yup.string()
-    .required('Required'),
-  addressLine2: Yup.string(),
-  city: Yup.string()
-    .required('Required'),
-  state: Yup.string()
-    .required('Required'),
-  country: Yup.string()
-    .required('Required'),
-  arrivealDate: Yup.date()
-    .required('Required'),
-  departureDate: Yup.date()
-    .required('Required'),
-  message: Yup.string(),
-  termsOfService: Yup.boolean()
-    .oneOf([true], 'The terms and conditions must be accepted.')
-    .required('The terms and conditions must be accepted.'),
-});
+import * as React from 'react';
+import { TextField, Button, Box, Grid, Container } from '@mui/material';
+import { useForm, Controller } from "react-hook-form";
+import "./App.css"
 
 const App = () => {
-  const classes = useStyles();
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: ''
+    }
+  });
+
+  const onSubmit = data => console.log(data);
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Header />
-      </Grid>
-      <Grid item xs={12}>
-        <Container maxWidth="md">
-          <div className={classes.formWrapper}>
-            <Formik
-              initialValues={{
-                ...INITIAL_FORM_STATE
-              }}
-              validationSchema={FORM_VALIDATION}
-              onSubmit={values => {
-                console.log(values);
-              }}
-            >
-              <Form>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Typography>
-                      Your details
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="firstName"
-                      label="First Name"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="lastName"
-                      label="Last Name"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="email"
-                      label="Email"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="phone"
-                      label="Phone"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Typography>
-                      Address
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="addressLine1"
-                      label="Address Line 1"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="addressLine2"
-                      label="Address Line 2"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="city"
-                      label="City"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <Textfield
-                      name="state"
-                      label="State"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Select
-                      name="country"
-                      label="Country"
-                      options={countries}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Typography>
-                      Booking information
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <DateTimePicker
-                      name="arrivealDate"
-                      label="Arrival Date"
-                    />
-                  </Grid>
-
-                  <Grid item xs={6}>
-                    <DateTimePicker
-                      name="departureDate"
-                      label="Departure Date"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Textfield
-                      name="message"
-                      label="Message"
-                      multiline={true}
-                      rows={4}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Checkbox
-                      name="termsOfService"
-                      legend="Terms Of Service"
-                      label="I agree"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Button>
-                      Submit Form
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Form>
-            </Formik>
-          </div>
-        </Container>
-      </Grid>
-    </Grid>
+    <Container component="main" maxWidth="xs">
+        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+            <Controller
+              name="firstName"
+              control={control}
+              render={({ field }) => <TextField 
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoFocus
+              {...field} />}
+              />
+              <Button variant='contained' color='primary' type="submit">
+                Sign Up
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
   );
 };
 
