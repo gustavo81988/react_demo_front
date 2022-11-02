2a746dd67902ac6e1a132f17284c14ebe0050bb3
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-02 10:19:23 -0400

    avance

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { TextField, Button, Box, Grid, Container } from '@mui/material';
 import { useForm, Controller } from "react-hook-form";
 import "./App.css"
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const App = () => {
   const { control, handleSubmit } = useForm({
@@ -14,64 +17,74 @@ const App = () => {
   const onSubmit = data => console.log(data);
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name="firstName"
-                control={control}
-                render={({ field }) => <TextField 
-                margin="normal"
-                fullWidth
-                label="First Name"
-                name="las_name"
-                autoFocus
-                {...field} />}
-              />
-            </Grid>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+        >
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                  <Controller
+                    name="firstName"
+                    control={control}
+                    render={({ field }) => <TextField 
+                    margin="normal"
+                    fullWidth
+                    label="First Name"
+                    name="las_name"
+                    autoFocus
+                    {...field} />}
+                  />
+                </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name="lastName"
-                control={control}
-                render={({ field }) => <TextField 
-                margin="normal"
-                fullWidth
-                label="Last Name"
-                name="las_name"
-                autoFocus
-                {...field} />}
-              />
-            </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Controller
+                    name="lastName"
+                    control={control}
+                    render={({ field }) => <TextField 
+                    margin="normal"
+                    fullWidth
+                    label="Last Name"
+                    name="las_name"
+                    autoFocus
+                    {...field} />}
+                  />
+                </Grid>
 
-            <Grid item xs={12} sm={12}>
-              <Controller
-                name="email"
-                control={control}
-                render={({ field }) => <TextField 
-                margin="normal"
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoFocus
-                {...field} />}
-              />
+                <Grid item xs={12} sm={12}>
+                  <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => <TextField 
+                    margin="normal"
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoFocus
+                    {...field} />}
+                  />
+                </Grid>
             </Grid>
-              
             <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+                Sign Up
             </Button>
-          </Grid>
+          </Box>
         </Box>
       </Container>
+    </ThemeProvider>
   );
 };
 
