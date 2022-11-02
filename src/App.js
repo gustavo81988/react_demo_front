8606c1addf4f512a115fed6c0eb547e2d9044de5
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-02 10:04:46 -0400

    avance

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { TextField, Button, Box, Grid, Container } from '@mui/material';
 import { useForm, Controller } from "react-hook-form";
 import "./App.css"
+import CssBaseline from '@mui/material/CssBaseline';
 
 const App = () => {
   const { control, handleSubmit } = useForm({
@@ -14,25 +15,60 @@ const App = () => {
 
   return (
     <Container component="main" maxWidth="xs">
+      <CssBaseline />
         <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="firstName"
+                control={control}
+                render={({ field }) => <TextField 
+                margin="normal"
+                fullWidth
+                label="First Name"
+                name="las_name"
+                autoFocus
+                {...field} />}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="lastName"
+                control={control}
+                render={({ field }) => <TextField 
+                margin="normal"
+                fullWidth
+                label="Last Name"
+                name="las_name"
+                autoFocus
+                {...field} />}
+              />
+            </Grid>
+
             <Grid item xs={12} sm={12}>
-            <Controller
-              name="firstName"
-              control={control}
-              render={({ field }) => <TextField 
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => <TextField 
                 margin="normal"
                 fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
                 autoFocus
-              {...field} />}
+                {...field} />}
               />
-              <Button variant='contained' color='primary' type="submit">
-                Sign Up
-              </Button>
             </Grid>
+              
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
           </Grid>
         </Box>
       </Container>
