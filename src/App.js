8c5c6da740475560ee1306680e207d214e44636d
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-06 17:21:25 -0400

    top bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
 import { Routes, Route } from "react-router-dom";
 import SignUp from './scenes/auth/SignUp';
 import SignIn from './scenes/auth/SignIn';
 
 const App = () => {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/sign_up" element={<SignUp />} />
-    </Routes>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
