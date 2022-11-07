4e2c3d39836bbd8e4758a3d31b0a24b680ae00c2
Gustavo Ramirez <gramirez@overall.com.pe>
2022-11-07 19:49:35 -0400

    sign in condition

--- src/App.js
+++ src/App.js
@@ -10,21 +10,30 @@ import SignIn from './scenes/auth/SignIn';
 const App = () => {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  let isLoggedIn = false;
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <>
+      {
+        isLoggedIn? 
+          <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <div className="app">
+                <Sidebar isSidebar={isSidebar} />
+                <main className="content">
+                  <Topbar setIsSidebar={setIsSidebar} />
+                  <Routes>
+                  </Routes>
+                </main>
+              </div>
+            </ThemeProvider>
+          </ColorModeContext.Provider>
+        : 
+          <SignIn/>
+      }
+    </>
+    
   );
 };
 
